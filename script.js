7f117fe45886a7ba7982a58cccceb755c1eec345
SebastianTong <tongsebastian27@gmail.com>
2018-04-13 17:17:05 -0300

    asd

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-// Funtcion constructor
-/*
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
-};
-
-var Person = function(name, yearOfBirth, job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-}
-
-Person.prototype.calculateAge = function(){
-    console.log(2018 - this.yearOfBirth);
-}
-
-var seba = new Person('Seba', 1994, 'developer');
-var jane = new Person('Jane', 1969, 'designer');
-var mark = new Person('Mark', 1948, 'retired');
-
-seba.calculateAge();
-jane.calculateAge();
-mark.calculateAge();
-
-
-//Object.create
-
-var personProto = {
-    calculateAge: function(){
-        console.log(2018 - this.yearOfBirth);
-    }
-};
-
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
-
-var jane = Object.create(personProto, {
-    name: { value: 'Jane'},
-    yearOfBirth: { value: 1969 },
-    job: { value: 'designer' }
-});
-
-*/
-
 // Primitives vs. Objects
 
 
@@ -87,5 +39,4 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
-
 
