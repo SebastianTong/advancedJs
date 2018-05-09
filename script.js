4157cbf71f94b6d5ad850274b850c40a966e2e19
SebastianTong <tongsebastian27@gmail.com>
2018-05-09 18:26:04 -0300

    asd

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-// Lecture: Closures
-
-/*
-
-function retirement(retirementAge) {
-    var a = ' years until retirement.';
-    return function(yearOfBirth){
-        var age = 2018 - yearOfBirth;
-        console.log((retirementAge - age) + a);
+var john = {
+    name: 'John',
+    age: 26,
+    job: 'teacher',
+    presentation: function (style, timeOfDay) {
+        if (style === 'formal') {
+            console.log('Good ' + timeOfDay + ' ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if (style === 'friendly') {
+            console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
     }
-}
+};
 
-var retirementUS = retirement(66);
-var retirementGermany = retirement(65);
-var retirementIceland = retirement(67);
+john.presentation('formal', 'morning');
 
-retirementGermany(1990);
-retirementUS(1990);
-retirementIceland(1990);
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
 
-*/
+john.presentation.call(emily, 'friendly', 'afternoon');
 
+// john.presentation.apply(emily,['friendly', 'afternoon']);
+//This is the apply method with allows parameters such as arrays.
+
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+
+emilyFormal('afternoon');
+
+//This up here is the bind method, which returns a function, so you can preset some parameters.
